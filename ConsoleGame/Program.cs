7a6ac9b238b28be7ba4fb3a14fadef9d5ae9c874
Findley Griffiths <findleygriffiths@gmail.com>
2022-01-14 17:09:54 -0500

    Initial commit

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using ConsoleGameClasses;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            int allowedWrongGuesses = 7;
+            string wordListLocation = "ADDLOCATION\\assets\\words.txt";
+
+            Console.Clear();
+            Hangman game = new Hangman();
+            char playAgain = 'y';
+            bool playGame = true;
+
+            while (playGame)
+
+            {
+                game.Instructions(allowedWrongGuesses, letters);
+
+                string pickedWord = game.GetWord(wordListLocation);
+
+                char[] pickedArray = pickedWord.ToCharArray();
+                int pickedWordLength = pickedWord.Length;
+
+                char[] guessingArray = new char[pickedWordLength];
+
+                for (int i = 0; i < pickedWordLength; i++)
+                {
+                    guessingArray[i] = '-';
+                }
+                char[] letterList = letters.ToCharArray();
+
+                game.HangmanGame(allowedWrongGuesses, pickedWordLength, guessingArray, pickedArray, letterList);
+
+                Console.WriteLine("Would you like to play again?  Press \"y\" to continue, any other key to exit.");
+                playAgain = Console.ReadKey().KeyChar;
+                if (playAgain == 'y')
+                {
+                    Console.Clear();
+                    playGame = true;
+                }
+                else 
+                {
+                    playGame = false;
+                }
+               
+            }
+        }
+    }
+}
+// ✓✓✓ Need to: Instructions
+// ✓✓✓ Need to: Create a random number generator to pick the word 
+// ✓✓✓ Need to: Display underlines corresponding to character count, after word is picked
+// Need to: Make sure guesses are converted to lower case
+// ✓✓✓ Need to: Display the letter that is picked in the underlines
+// ✓✓✓ Need to: Develop logic to see if letter is in the word
+// ✓✓✓ Need to: Let player know how many guesses are left 
+//                                                              (Optional): Put a graphic of hangman's body
+// ✓✓✓ Need to: Develop logic to see if game is won or lost (if guesses = 0, if word is solved)
+// ✓✓✓ Need to: Display messages to player based on winning or losing
+// Need to: (Optional) Have a list of available letters left to pick (Have an error message if already guessed?)
+// Need to: (Optional) Give player an option to restart with a new word/ "Play again"
+
+/*
+
+Written by:
+
+Findley Griffiths
+Elijah Beach
+Alan Murugan
+
+*/
---
+++ ConsoleGameClasses/Hangman.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleGameClasses
+{
+
+    public class Hangman
+    {
+
+        public void Instructions(int allowedWrongGuesses, string letters)
+        {
+            Console.WriteLine("BOMB SQUAD!");
+            Console.WriteLine("Guess the password correctly to defuse the bomb, one letter at a time!");
+            Console.WriteLine($"Be careful though, {allowedWrongGuesses} wrong guesses and you explode!\n");
+            Console.WriteLine($"Remaining Letters: {letters}");
+        }
+
+        public string GetWord(string wordlistLocation)
+        {
+            // import the wordlist and split by carriage return
+            string wordList = System.IO.File.ReadAllText(wordlistLocation);
+            string[] wordArray = wordList.Split('\n');
+
+            // obtain number for random number generator
+            int lineCount = wordArray.Count();
+
+            // generate the number and pick the word from the array
+            Random rnd = new Random();
+            int pickedIndex = rnd.Next(0, lineCount);
+            string pickedWord = wordArray[pickedIndex];
+            // Console.WriteLine("working");
+            return pickedWord;
+        }
+
+
+        public string DisplayLetters(char guess, char[] letterList)
+        {
+            // Run through the word to return visual indicator of length
+            for (int i = 0; i < letterList.Length; i++)
+            {
+                if (letterList[i] == guess)
+                {
+                    letterList[i] = '-';
+                }
+
+            }
+            string remainingLetters = new string(letterList);
+            return remainingLetters;  // sends the array back with updated information
+
+        }
+
+        public void HangmanGame(int allowedWrongGuesses, int pickedWordLength, char[] guessingArray, char[] pickedArray, char[] letterList)
+        {
+            while (allowedWrongGuesses > 0 && Enumerable.SequenceEqual(guessingArray, pickedArray) == false)
+            {
+                
+                // Build the bomb
+                string fuse = new string('-', allowedWrongGuesses);
+                Console.WriteLine("  __");
+                Console.WriteLine(" /  \\");
+                Console.WriteLine("|    |" + fuse + "X");
+                Console.WriteLine(" \\__/\n");
+
+                // Get the guesses
+                Console.Write(guessingArray);
+                Console.Write('\n');
+                Console.Write("Enter your guess: ");
+                char guess = Console.ReadKey().KeyChar;
+                Console.Clear();
+                Console.WriteLine('\n');
+
+                // Show the remaining letters
+                Console.WriteLine($"Remaining Letters:\n{DisplayLetters(guess, letterList)}\n");
+
+
+                // Check to see if the word is contained in the picked word array
+                bool correctGuess = Array.Exists(pickedArray, element => element == guess);
+
+                if (correctGuess)
+                {
+                    Console.WriteLine($"You have survived another round. Only {allowedWrongGuesses} wrong guesses left.");
+                    for (int i = 0; i < pickedWordLength; i++)
+                    {
+                        if (guess == pickedArray[i])
+                        {
+                            guessingArray[i] = guess;
+                        }
+                    }
+                
+                    // set the game winning condition
+                    if (Enumerable.SequenceEqual(guessingArray, pickedArray))
+                    {
+                        Console.WriteLine("You have defused the bomb! You didn't die!");
+                    }
+                }
+                else
+                {
+                    allowedWrongGuesses--;
+                    if (allowedWrongGuesses == 0)
+                    {
+                        string correctWord = new string(pickedArray);
+                        Console.WriteLine("#####    ####    ####   #     #");
+                        Console.WriteLine("#    #  #    #  #    #  ##   ##");
+                        Console.WriteLine("#    #  #    #  #    #  # # # #");
+                        Console.WriteLine("#####   #    #  #    #  # # # #");
+                        Console.WriteLine("#    #  #    #  #    #  #  #  #");
+                        Console.WriteLine("#    #  #    #  #    #  #  #  #");
+                        Console.WriteLine("#####    ####    ####   #     #\n");
+                        Console.WriteLine($"YOU BLEW UP!!! The word was {correctWord}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Careful, only {allowedWrongGuesses} incorrect guesses left!");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
